706739fa4ec126b76d7595f3d66e78268b63b7f1
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-05-12 00:39:24 +0300

    pulse check 3-5

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const fs = require("fs");
+const { response } = require("express");
 
 const app = express();
 const PORT = 3000;
@@ -13,7 +14,33 @@ const readFile = () => {
         content = data.toString();
         console.log(content);
     })
+};
+
+const writeFile = () => {
+    fs.writeFile("text.txt", "A new file has been created", (err)=>{
+        if (err) throw err;
+        console.log("The file has been saved");
+    })
+};
+
+const getPost = (id) => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((response)=>{
+        console.log(response.data);
+    })
+    .catch((err)=>{
+        throw err
+    })
   };
+
+const getPostAsync = async (data) => {
+    try {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${data}`)
+        console.log(`response.data`);
+    } catch (err){
+        throw err
+    }
+};
 
 app.listen(PORT, ()=>{
     console.log("hello world");
